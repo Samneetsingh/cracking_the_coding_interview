5fb812a46cb1c6098e7a064dffaef0e2990eb74a
samneets <samneets@ciena.com>
2019-11-10 14:03:23 -0500

    Wrote is_unique method with out additional data structure

--- src/collection/lists/node.py
+++ src/collection/lists/node.py
@@ -1,6 +1,6 @@
 class Node(object):
 
-    def __init__(self, info) -> None:
+    def __init__(self, info: int) -> None:
         self.__info = info
         self.__next = None
 
@@ -8,13 +8,31 @@ class Node(object):
         return "{}".format(self.__info)
 
     def get_next(self) -> object:
+        """
+        Function to get next node
+        :return: Node
+        """
         return self.__next
 
     def get_info(self) -> int:
+        """
+        Function to get info
+        :return: info
+        """
         return self.__info
 
     def set_next(self, link: object) -> None:
+        """
+        Function to set next node
+        :param link: node
+        :return: None
+        """
         self.__next = link
 
     def set_info(self, info: int) -> None:
+        """
+        Function to set info
+        :param info: info
+        :return: None
+        """
         self.__info = info
--- src/collection/lists/singlyLinkedList.py
+++ src/collection/lists/singlyLinkedList.py
@@ -3,18 +3,28 @@ from src.collection.lists.node import Node
 
 class SinglyLinkedList(object):
 
-    def __init__(self, info: int) -> None:
+    def __init__(self, info) -> None:
         self.head = Node(info)
         self.tail = self.head
         self.size = 1
 
-    def add_link(self, info: int) -> None:
+    def add_link(self, info) -> None:
+        """
+        Function to add a new link to linkedlist
+        :param info: data
+        :return: None
+        """
         new_node = Node(info)
         self.tail.set_next(new_node)
         self.tail = new_node
         self.size += 1
 
-    def remove_link(self, info: int) -> None:
+    def remove_link(self, info) -> None:
+        """
+        Function to remove a new link from linkedlist
+        :param info: data
+        :return: None
+        """
         prev_node = None
         next_node = self.head
         while next_node is not None:
@@ -28,11 +38,16 @@ class SinglyLinkedList(object):
             next_node = next_node.get_next()
 
     def print_links(self) -> None:
+        """
+        Function to print linkedlist
+        :return: None
+        """
         next_node = self.head
-        while next_node is not None:
-            print("{},".format(next_node.get_info()), end="\t")
+        print("|", end="")
+        while next_node.get_next() is not None:
+            print("{}".format(next_node.get_info()), end="|")
             next_node = next_node.get_next()
-        print("\n")
+        print("{}|".format(next_node.get_info()))
 
 
 if __name__ == '__main__':
@@ -41,6 +56,6 @@ if __name__ == '__main__':
     test_list.add_link(info=3)
     test_list.add_link(info=4)
     test_list.remove_link(info=5)
-    test_list.remove_link(info=6)
+    test_list.remove_link(info=1)
     test_list.remove_link(info=7)
     test_list.print_links()
