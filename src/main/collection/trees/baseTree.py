96bb37cf512cd488c7d9fda77ab2c8361301345b
samneets <samneets@ciena.com>
2019-12-23 09:39:13 +0530

    Added AVL tree and its tests

--- src/main/collection/trees/baseTree.py
+++ src/main/collection/trees/baseTree.py
@@ -6,12 +6,21 @@ class BaseTree(abc.ABC):
 
     @abc.abstractmethod
     def insert(self, info: Any) -> None:
+        """
+        Abstract function to insert node into a tree
+        """
         pass
 
     @abc.abstractmethod
     def remove(self, info: Any) -> None:
+        """
+        Abstract function to remove node from a tree
+        """
         pass
 
     @abc.abstractmethod
     def traverse(self, name: str) -> None:
+        """
+        Abstract function to traverse a tree
+        """
         pass
